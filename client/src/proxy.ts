8c305d9d0769c507a38a1c1a7227e10ecc651368
Kawsar37 <kawsarali750@gmail.com>
2026-07-18 21:24:33 +0600

    proxy.ts on hold

--- client/src/proxy.ts
+++ client/src/proxy.ts
@@ -4,13 +4,13 @@ import { headers } from "next/headers";
 import { auth } from "./lib/auth";
 
 export async function proxy(request: NextRequest) {
-  const session = await auth.api.getSession({
-    query: {
-      disableCookieCache: true,
-    },
-    headers: await headers(),
-  });
-  if (!session) return NextResponse.redirect(new URL("/login", request.url));
+  // const session = await auth.api.getSession({
+  //   query: {
+  //     disableCookieCache: true,
+  //   },
+  //   headers: await headers(),
+  // });
+  // if (!session) return NextResponse.redirect(new URL("/login", request.url));
   return NextResponse.next();
 }
 
