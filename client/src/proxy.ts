f74362a1d46bf2ef08fd6ac40a9a3932a9825d65
Kawsar37 <kawsarali750@gmail.com>
2026-07-18 13:36:13 +0600

    proxy.ts fixed

--- client/src/proxy.ts
+++ client/src/proxy.ts
@@ -1,7 +1,16 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
+import { headers } from "next/headers";
+import { auth } from "./lib/auth";
 
-export function proxy(request: NextRequest) {
+export async function proxy(request: NextRequest) {
+  const session = await auth.api.getSession({
+    query: {
+      disableCookieCache: true,
+    },
+    headers: await headers(),
+  });
+  if (!session) return NextResponse.redirect(new URL("/login", request.url));
   return NextResponse.next();
 }
 
